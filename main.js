c423d610f266ab284dfb2f4eb52fc963e13ca969
梦念幻想 <5402106+imohuan@user.noreply.gitee.com>
2022-11-07 20:12:19 +0800

    xxx

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 module.exports = {
-  hello() {
-    return "更新版本 1.0.5";
-  },
+  apply(hooks) {
+    hooks.add("extension-init", (...args) => console.log("hello init", ...args));
+    hooks.add("extension-destroy", (...args) => console.log("hello destroy", ...args));
+    hooks.add("extension-progress", (...args) => console.log("hello progress", ...args));
+
+    return {
+      getVersion: () => console.log("更新版本 1.0.5")
+    };
+  }
 };
